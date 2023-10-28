b88fd0f9e3e9165bf141bc202db7b119d5836f2c
Aashika Choudhary <a.choudhary12@lse.ac.uk>
2023-10-28 17:43:21 +0100

    Adding the squaring column one function

---
+++ Assignment2_function.R
@@ -0,0 +1,6 @@
+# Below, I have made a function called square_column which is squaring the first column of any datatset.
+# This will only return something meaningful if the column contains numeric data
+square_column <- function(data) {
+  column_one_square <- data[1] * data[1]
+  return(column_one_square)
+}
